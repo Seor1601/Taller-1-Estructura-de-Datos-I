7c2c1a06eabcac1b74579830747ec32d79a256bd
Samuel Elias Ortega Rodriguez <samuelortega1601@gmail.com>
2026-02-09 07:44:31 -0500

    Revert "Feature/calculos validaciones"

--- BibliotecaApp.java
+++ BibliotecaApp.java
@@ -19,10 +19,7 @@ public class BibliotecaApp {
                 case 3 -> buscarPrestamoPorId();
                 case 4 -> actualizarPrestamo();
                 case 5 -> eliminarPrestamo();
-                    //Eliminar un préstamo de la lista usando su ID.
                 case 6 -> calcularTotalMultas();
-                    //Calcular y mostrar el total estimado de multas:
-                    //diasPrestamo × multaPorDia
                 case 7 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida.");
             }
@@ -48,50 +45,11 @@ public class BibliotecaApp {
     static void mostrarPrestamos() { /* TODO */ }
     static void buscarPrestamoPorId() { /* TODO */ }
     static void actualizarPrestamo() { /* TODO */ }
-
-    static void eliminarPrestamo() {
-        int id = leerEntero("Ingrese el ID del préstamo a eliminar: ");
-        for (int i = 0; i < prestamos.size(); i++) {
-            ArrayList<Object> p = prestamos.get(i);
-            if (p == null || p.isEmpty()) continue;
-            Object idObj = p.get(0);
-            int idPrestamo;
-            if (idObj instanceof Integer) {
-                idPrestamo = (Integer) idObj;
-            } else {
-                try {
-                    idPrestamo = Integer.parseInt(idObj.toString());
-                } catch (Exception e) {
-                    continue;
-                }
-            }
-            if (idPrestamo == id) {
-                prestamos.remove(i);
-                System.out.println("Préstamo con ID " + id + " eliminado.");
-                return;
-            }
-        }
-        System.out.println("No se encontró un préstamo con ID " + id + ".");
-    }
+    static void eliminarPrestamo() { /* TODO */ }
 
     // ====== Cálculo (por implementar) ======
-    static void calcularTotalMultas() { 
-        int totalMultas = 0;
-        for (ArrayList<Object> p : prestamos) {
-            if (p == null || p.size() < 5) continue;
-            Object diasObj = p.get(3);
-            Object multaObj = p.get(4);
-            int diasPrestamo, multaPorDia;
-            try {
-                diasPrestamo = Integer.parseInt(diasObj.toString());
-                multaPorDia = Integer.parseInt(multaObj.toString());
-            } catch (Exception e) {
-                continue;
-            }
-            totalMultas += diasPrestamo * multaPorDia;
-        }
-        System.out.println("Total estimado de multas: " + totalMultas);
-     }
+    static void calcularTotalMultas() { /* TODO */ }
+
     // ====== Utilidades mínimas ======
     static int leerEntero(String msg) {
         while (true) {
